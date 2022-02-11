d26dbf64906ba7d6333f69f12b29ba05d9fe28f9
Aqib Mustafa <aqmustafa@paypal.com>
2022-02-12 00:39:59 +0530

    Hospital API

---
+++ src/main/java/com/aqib/hospital/model/Diagnosis.java
@@ -0,0 +1,19 @@
+package com.aqib.hospital.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@ToString
+@Data
+@NoArgsConstructor
+public class Diagnosis {
+    String id;
+    String referredHospital;
+    String referredDoc;
+    List<String> doctorDiagnosis;
+}
